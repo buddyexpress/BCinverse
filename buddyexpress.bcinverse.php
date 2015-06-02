27e34bc88d86f355b86b520c55a6f38e578e4ade
Arsalan Shah <manactric@gmail.com>
2015-06-02 13:51:10 +0500

    Update buddyexpress.bcinverse.php

--- buddyexpress.bcinverse.php
+++ buddyexpress.bcinverse.php
@@ -14,11 +14,11 @@
  */
 //(get::apache_modules('buddyexpress') && get::php_extension('buddyexpress'))==run
 /**
-  BCinverse Get Variables and Replace
-  $get_input = a method which you are getting input, GET or POST or REQIEST.
-  Make sure getting input is secure.
-  $params = which character is need to replace with what.
-*/ 
+ * BCinverse Get Variables and Replace
+ * $get_input = a method which you are getting input, GET or POST or REQIEST.
+ * Make sure getting input is secure.
+ * $params = which character is need to replace with what.
+ */ 
 ini_set('display_errors','off'); 
 function buddyexpress_bcinverse($get_input, $params = array(), $method = NULL){
 if($method  == 1){$strc = $get_input;} else {$strc = strtolower($get_input); } 
@@ -29,12 +29,12 @@ if($method  == 1){$strc = $get_input;} else {$strc = strtolower($get_input); }
 		return $result;
 }
 /**
-  BCinverse chars list function
-  $get_input = a method which you are used for getting input, GET or POST or REQUEST.
-  Make sure getting input is secure.
-  We used html entities.
-  return = result;
-*/ 
+ * BCinverse chars list function
+ * $get_input = a method which you are used for getting input, GET or POST or REQUEST.
+ * Make sure getting input is secure.
+ * We used html entities.
+ * return = result;
+ */ 
 function buddyexpress_bcinverse_result($get_input = ''){
 if(!empty($get_input)){
 $params = array('a' => '&#592;','b' => 'q','c' => '&#596;','e' => '&#477;','f' => '&#607;','g' => '&#387;','h' => '&#613;','i' => '&#305;','j' => '&#638;','k' => '&#670;','l' => 'l','m' => '&#623;','n' => 'u','o' => 'o','p' => 'd','q' => 'b','r' => '&#633;',
@@ -43,11 +43,11 @@ $params = array('a' => '&#592;','b' => 'q','c' => '&#596;','e' => '&#477;','f' =
     }
 }
 /**
-  BCinverse chars list function
-  $get_input = a method which you are used for getting input, GET or POST or REQUEST.
-  Make sure getting input is secure.
-  return = result;
-*/ 
+ * BCinverse chars list function
+ * $get_input = a method which you are used for getting input, GET or POST or REQUEST.
+ * Make sure getting input is secure.
+ * return = result;
+ */ 
 function buddyexpress_bcinverse_reset($get_input = ''){
 if(!empty($get_input)){
 $params = array('ɐ' => 'a','q' => 'b','ɔ' => 'c','p' => 'd','ǝ' => 'e','ɟ' => 'f','ƃ' => 'g','ɥ' => 'h','ı' => 'i','ɾ' => 'j','ʞ' => 'k','l' => 'l','ɯ' => 'm','u' => 'n','o' => 'o','d' => 'p','b' => 'q','ɹ' => 'r','ʇ' => 't','n' => 'u','ʌ' => 'v','ʍ' => 'w',
